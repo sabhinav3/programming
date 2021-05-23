a69eb56a9bee7f04e0eb65945c62bddaee79e35d
S Abhinav <60837594+sabhinav3@users.noreply.github.com>
2021-05-23 13:02:13 +0530

    Create stdin_and_stdout_II.java

---
+++ HackerRank/stdin_and_stdout_II.java
@@ -0,0 +1,21 @@
+/*
+Question Link : https://www.hackerrank.com/challenges/java-stdin-stdout/problem?h_r=profile
+*/
+
+// Soluton 
+
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int i = scan.nextInt();
+
+        // Write your code here.
+
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+    }
+}
