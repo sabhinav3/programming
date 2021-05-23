18e176097ed5adb172c869c30a60971019386a78
S Abhinav <60837594+sabhinav3@users.noreply.github.com>
2021-05-23 13:11:28 +0530

    Create playing_with_characters.c

---
+++ HackerRank/playing_with_characters.c
@@ -0,0 +1,21 @@
+/*
+Question Link : https://www.hackerrank.com/challenges/playing-with-characters/problem
+*/
+
+// Solution 
+
+
+#include<stdio.h>
+int main(void)
+{
+    char word1[100],word2[100],word3[100];
+    scanf("%[^\n]%*c",word1);
+    scanf("%[^\n]%*c",word2);
+    scanf("%[^\n]%*c",word3);
+    printf("%s \n",word1);
+    printf("%s \n",word2);
+    printf("%s \n",word3);
+    
+
+}
+
