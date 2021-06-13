ae935d9b73d4335942ce5cbd3fb29e5f2266f41f
S Abhinav <60837594+sabhinav3@users.noreply.github.com>
2021-06-13 17:53:16 +0530

    Create kcal.java

---
+++ Atcoder/kcal.java
@@ -0,0 +1,25 @@
+/*
+
+AtCoder Beginner Contest 205
+Date : 13/06/2021
+Contest Link : https://atcoder.jp/contests/abc205
+Problem Link : https://atcoder.jp/contests/abc205/tasks/abc205_a
+
+*/
+
+// Solution
+
+import java.util.*;
+public class Main{
+
+  public static void main(String[] args) { 
+    	Scanner input = new Scanner(System.in); 
+        int a = input.nextInt();
+        int b = input.nextInt(); 
+
+        float x = (float)(a*b)/100;
+
+        System.out.println(x);
+
+    } // main function
+}
