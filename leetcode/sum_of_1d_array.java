66c256550a063184019f750377097099031c6d1c
S Abhinav <60837594+sabhinav3@users.noreply.github.com>
2021-06-29 18:52:40 +0530

    Update sum_of_1d_array.java

--- leetcode/sum_of_1d_array.java
+++ leetcode/sum_of_1d_array.java
@@ -1,3 +1,13 @@
+/*
+
+Platform : Leetcode
+Problem Name : 1480. Running Sum of 1d Array
+Problem Link : https://leetcode.com/problems/running-sum-of-1d-array/
+
+
+*/
+
+
 class Solution {
     public int[] runningSum(int[] nums) {
         int i, ans = 0;
