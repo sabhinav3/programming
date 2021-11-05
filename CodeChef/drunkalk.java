597774210613e11ec6ba37c5ac75e89018500c8d
S Abhinav <60837594+sabhinav3@users.noreply.github.com>
2021-11-05 08:51:35 +0530

    Create drunkalk.java

---
+++ CodeChef/drunkalk.java
@@ -0,0 +1,40 @@
+import java.util.*;
+public class drunkalk{
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int t = input.nextInt(); // no.of test cases
+		int n_array[] = new int[t]; // denoting total no.of steps
+		int i,j, sum=0;
+		int sum_array[] = new int[t];
+
+		for(i=0; i<t; i++){
+			n_array[i] = input.nextInt();
+		}
+
+
+		for(i=0; i<t; i++){
+			for(j=1; j<=n_array[i]; j++){
+
+				if(j==1){
+					sum = sum + 3;
+				}
+
+				else if(j%2!=0){
+					sum = sum+3;
+				}
+
+				else{
+					sum = sum-1;
+				}
+			}
+			
+			sum_array[i] = sum;
+			sum = 0;
+		}
+
+		for(i=0; i<t; i++){
+			System.out.println(sum_array[i]);
+			
+		}
+	}
+}
