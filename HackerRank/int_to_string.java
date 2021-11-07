7cd88377588b4da3337f3d22d7e9de22734cd256
S Abhinav <60837594+sabhinav3@users.noreply.github.com>
2021-11-07 16:33:32 +0530

    Create int_to_string.java

---
+++ HackerRank/int_to_string.java
@@ -0,0 +1,22 @@
+
+
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner input = new Scanner(System.in);
+        int i = input.nextInt();
+        String s = Integer.toString(i);
+        if((i>=-100)||(i<=100)){
+            System.out.println("Good job");
+        }
+        
+        else{
+            System.out.println("Wrong answer");
+        }
+    }
+}
